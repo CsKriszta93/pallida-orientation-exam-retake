448ee13ffff39fa698fb230299998579a225b6ff
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-27 14:07:57 +0100

    View with dropdown select added

--- Clothing/Controllers/HomeController.cs
+++ Clothing/Controllers/HomeController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Clothing.Repositories;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Clothing.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        ClothRepository clothRepository;
+
+        public HomeController(ClothRepository clothRepository)
         {
-            return View();
+            this.clothRepository = clothRepository;
+        }
+
+        [Route("/warehouse")]
+        public IActionResult Clothes()
+        {
+            return View(clothRepository.ListOfClothes());
         }
     }
 }
