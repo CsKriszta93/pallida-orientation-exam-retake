645746507de26a68cfb64be42b9b636ab1ef1ce0
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-27 13:25:15 +0100

    ClothContext added

---
+++ Clothing/Entities/ClothContext.cs
@@ -0,0 +1,19 @@
+﻿using Clothing.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clothing.Entities
+{
+    public class ClothContext : DbContext
+    {
+        public ClothContext(DbContextOptions<ClothContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Clothes> warehouse {get; set;}
+    }
+}
---
+++ Clothing/Models/Clothes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clothing.Models
+{
+    public class Clothes
+    {
+        public long Id { get; set; }
+        public string item_name { get; set; } 
+        public string manufacturer { get; set; }
+        public string category { get; set; }
+        public string size { get; set; }
+        public double unit_price { get; set; }
+    }
+}
